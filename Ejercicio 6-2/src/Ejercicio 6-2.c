6a8b4f371609ac662ff42c860a2dd13da03d7b65
AgosBeggi <you@example.com>
2021-09-27 19:27:50 -0300

    Resolucion Ejercicio 6-2

--- Ejercicio 6-2/src/Ejercicio 6-2.c
+++ Ejercicio 6-2/src/Ejercicio 6-2.c
@@ -9,18 +9,55 @@
  */
 
 /*
- Ejercicio 6-2:
+Ejercicio 6-2:
 Realizar un programa que permita el ingreso de 10 números enteros (positivos y negativos).
 Necesito generar un listado de los números positivos de manera creciente
 y negativos de manera decreciente. (Como máximo 5 estructuras repetitivas)
+
+Entregado con bibliotecas.
 
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "Vector.h"
+#include "Ingresos.h"
+#include "Validar.h"
+#include "Listados.h"
+
+#define TAMANIO 10
+#define INICIO 0
 
 int main(void) {
 	setbuf(stdout, NULL);
+
+	int vector[TAMANIO];
+	int estadoCarga;
+	int estadoPositivos;
+	int estadoNegativos;
+
+	InicializarVectorNumeros(vector, TAMANIO, INICIO);
+
+	estadoCarga = CargaSecuencialVectorNumeroEntero(vector, TAMANIO, "Ingrese un numero\n");
+	if(estadoCarga == 1){
+			printf("Usted ingrespo los siguientes datos:.\n");
+			MostrarLista(vector, TAMANIO, INICIO);
+		}else{
+			printf("Error, vuelva a intentarlo.\n");
+		}
+
+	estadoPositivos = OrdenarVectorNumerosPositivosA_Z(vector, TAMANIO);
+	estadoNegativos = OrdenarVectorNumerosNegativosZ_A(vector, TAMANIO);
+	if(estadoPositivos == 1 && estadoNegativos == 1){
+			printf("El listado de los numeros positivos ordenados de manera creciente es:.\n");
+			MostrarListaPositivos(vector, TAMANIO, INICIO);
+			printf("El listado de los numeros negativos ordenados de manera decreciente es:.\n");
+			MostrarListaNegativos(vector, TAMANIO, INICIO);
+		}
+
 
 	return EXIT_SUCCESS;
 }
---
+++ Ejercicio 6-2/src/Ingresos.c
@@ -0,0 +1,131 @@
+/*
+ * Ingresos.c
+ *
+ *  Created on: 16 sep. 2021
+ *      Author: Beggi Agostina
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "Validar.h"
+
+#define LARGO_CADENA 1000
+
+int IngresoEntero(char mensaje[], int* puntero){
+	char numeroCadena [LARGO_CADENA];
+	int numero;
+	int estadoNumero;
+	int estadoBooleano;
+
+	estadoBooleano=0;
+
+	printf("%s", mensaje);
+	fflush(stdin);
+	//fgets(numeroCadena, strlen(numeroCadena),stdin); //este no me muestra el numero
+	gets(numeroCadena);
+	//scanf('%[^\n]', &numeroCadena); // este me tira error, con "" o con ''
+
+	estadoNumero = EsNumeroEntero(numeroCadena, strlen(numeroCadena));
+	if(estadoNumero == 1){
+		numero = atoi(numeroCadena);
+		*puntero = numero;
+		estadoBooleano = 1;
+	}
+
+	return estadoBooleano;
+}
+
+int IngresoFlotante(char mensaje[], float* puntero){
+	char numeroCadena [LARGO_CADENA];
+	float numero;
+	int estadoNumero;
+	int estadoBooleano;
+
+	estadoBooleano=0;
+
+	printf("%s", mensaje);
+	fflush(stdin);
+	gets(numeroCadena);
+
+	estadoNumero = EsNumeroFlotante(numeroCadena, LARGO_CADENA);
+	if(estadoNumero == 1){
+		numero = atof(numeroCadena);
+		*puntero = numero;
+		estadoBooleano = 1;
+	}
+
+	return estadoBooleano;
+}
+
+int IngresoEnteroConParametros(char mensaje[], int min, int max, int* puntero){
+	char numeroCadena [LARGO_CADENA];
+	int numero;
+	int estadoNumero;
+	int estadoNumeroRango;
+	int estadoBooleano;
+
+	estadoBooleano=0;
+
+	printf("%s", mensaje);
+	fflush(stdin);
+	gets(numeroCadena);
+
+	estadoNumero = EsNumeroEntero(numeroCadena, LARGO_CADENA);
+	if(estadoNumero == 1){
+		numero = atoi(numeroCadena);
+	}
+
+	estadoNumeroRango = EstaEnteroEnRango(numero, min, max);
+	if(estadoNumero == 1 && estadoNumeroRango == 1){
+		*puntero = numero;
+		estadoBooleano = 1;
+	}
+
+	return estadoBooleano;
+}
+
+int IngresoFlotanteConParametros(char mensaje[], float min, float max, float* puntero){
+	char numeroCadena [LARGO_CADENA];
+	float numero;
+	int estadoNumero;
+	int estadoNumeroRango;
+	int estadoBooleano;
+
+	estadoBooleano=0;
+
+	printf("%s", mensaje);
+	fflush(stdin);
+	gets(numeroCadena);
+
+	estadoNumero = EsNumeroFlotante(numeroCadena, LARGO_CADENA);
+	if(estadoNumero == 1){
+		numero = atof(numeroCadena);
+	}
+
+	estadoNumeroRango = EstaFlotanteEnRango(numero, min, max);
+	if(estadoNumero == 1 && estadoNumeroRango == 1){
+		*puntero = numero;
+		estadoBooleano = 1;
+	}
+
+	return estadoBooleano;
+}
+
+/*int IngresarCadena(char mensaje[], char* cadena[], int tamanio)
+{
+	//para terminarla hay que probar cómo funciona
+	int retorno;
+
+	retorno = 0;
+
+	printf("%s", mensaje);
+	fflush(stdin);
+	fgets(cadena, strlen(cadena),stdin);
+
+	retorno = 1;
+
+	return retorno;
+}*/
